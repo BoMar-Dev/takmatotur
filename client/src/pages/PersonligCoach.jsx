28aeff502eca2344d2e29daea8b39430424cd9c9
marcus nilsson <marcusnilsson2010@gmail.com>
2024-09-11 16:51:33 +0200

    adjusting the text on private coach page and also adding img

--- client/src/pages/PersonligCoach.jsx
+++ client/src/pages/PersonligCoach.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 // Import component
 import SEOHelmet from "../components/SEOHelment";
+import SkeletonLoader from "../components/SkeletonLoader";
 
 // Import the background image
 import backgroundImg from "../img/about/background.svg";
+import personalCoach from "../img/coach/personalCoach.webp";
 
 const PersonligCoach = () => {
+  const [loading, setLoading] = useState(true);
+
   return (
     <>
       <SEOHelmet
@@ -39,13 +44,32 @@ const PersonligCoach = () => {
           <article className="xl:text-left">
             <p className="text-base md:text-md xl:text-xl font-semibold p-3 mx-auto">
               Att löpträna på egen hand kan vara både utmanande och ibland
-              ensamt. Oavsett om du är en nybörjare som vill komma igång eller
-              en erfaren löpare som siktar på nya personliga rekord, kan privat
+              ensamt. Oavsett om du är nybörjare som vill komma igång eller en
+              erfaren löpare som siktar på nya personliga rekord, kan privat
               löpcoachning från oss vara nyckeln till att ta ditt löpande till
-              nästa nivå. Inspiration, stöd och motivation varje steg på vägen.
+              nästa nivå. Med inspiration, stöd och motivation finns vi med dig
+              varje steg på vägen.
             </p>
           </article>
         </div>
+
+        <aside className="p-5 flex items-center max-w-screen-xl mx-auto">
+          {loading && <SkeletonLoader height="100%" width="100%" />}
+          <img
+            src={personalCoach}
+            alt="Profilbild på Martin Gillgren löpcoach Tak Mat o Tur"
+            className={`rounded-lg transition-opacity duration-500 ${
+              loading ? "opacity-0" : "opacity-100"
+            }`}
+            onLoad={() => setLoading(false)}
+            style={{
+              width: "100%", // Keep the width at 100% of its container
+              height: "400px", // Set the height to your desired value, adjust as needed
+              objectFit: "cover", // Ensures the image fills the area while cropping if necessary
+              transition: "opacity 1s ease-in-out",
+            }}
+          />
+        </aside>
 
         <div className="max-w-screen-xl mx-auto p-3 mb-10">
           <section className="mb-6">
@@ -58,18 +82,29 @@ const PersonligCoach = () => {
                 o Tur är den individuella anpassningen. Vi skapar
                 träningsprogram som är exakt anpassade efter dina mål, din
                 nuvarande form och dina personliga förutsättningar. Har du en
-                dröm om att springa ditt första maraton? Eller kanske vill du
-                bara förbättra din kondition och springa för nöjes skull? Vi
-                hjälper dig att sätta realistiska och uppnåeliga mål och ger dig
-                verktygen för att nå dem.
+                dröm om att springa ditt första maraton? Kanske slå personliga
+                rekord? Eller kanske vill du bara förbättra din kondition och
+                springa för nöjes skull? Vi hjälper dig att sätta realistiska
+                och uppnåeliga mål och ger dig verktygen för att nå dem.
+              </p>
+
+              <h2 className="mb-2 font-semibold text-xl">Löpcoaching Online</h2>
+              <p className="mb-5">
+                För dig som föredrar flexibilitet och bekvämlighet erbjuder vi
+                även personlig löpcoaching online. Vår onlinecoaching ger dig
+                samma personliga anpassning och expertstöd som våra fysiska
+                träffar, men med friheten att träna var du än befinner dig.
+                Perfekt för dig som har en hektisk vardag eller bor på en annan
+                ort men ändå vill ha professionell vägledning!
               </p>
 
               <h2 className="mb-2 font-semibold text-xl">Teknisk Expertis</h2>
               <p className="mb-5">
                 Att ha någon som kan analysera din löpteknik och ge dig konkreta
-                råd är ovärderligt. Som en del av individuellt program ingår
-                filmning och analys av din löpteknik. Genom att arbeta med din
-                hållning, steglängd och fotisättning optimeras varje löpsteg.
+                råd är ovärderligt. Som en del av ditt individuella program
+                ingår filmning och analys av din löpteknik. Genom att arbeta med
+                din hållning, steglängd och fotisättning optimeras varje löpsteg
+                för att förbättra både effektivitet och minskad skaderisk.
               </p>
 
               <h2 className="mb-2 font-semibold text-xl">
@@ -83,7 +118,7 @@ const PersonligCoach = () => {
                 du startade din löparresa. Den personliga relationen du bygger
                 med din coach ger dig ett tryggt och motiverande stöd som håller
                 dig på rätt spår. Inledningsvis kommer vi ha ett möte där vi
-                pratar nuläge, mål men också mycket motivation.
+                pratar nuläge, mål men också mycket motivation..
               </p>
 
               <h2 className="mb-2 font-semibold text-xl">
@@ -98,13 +133,64 @@ const PersonligCoach = () => {
               </p>
             </div>
           </section>
-
-          <section className="p-3 text-center">
-            <p className="text-center md:w-[80%] lg:w-[60%] xl:w-[70%] mx-auto">
-              Gör er redo att snöra på skorna och följ med oss på en resa mot
-              bättre hälsa, ökad arbetsglädje och snabbare löpsteg!
+          <section className="text-center p-5 max-w-screen-md mx-auto">
+            <h2 className="text-xl font-semibold text-amber-700 mb-2">
+              Gör dig redo att snöra på skorna
+            </h2>
+            <p>
+              Följ med på en resa mot bättre hälsa, ökad arbetsglädje och
+              snabbare löpsteg – oavsett om det är på plats i Åredalen eller
+              online!
             </p>
           </section>
+
+          <div className="max-w-screen-xl mx-auto p-3 mb-10">
+            <section className="mb-6">
+              <div className="p-3">
+                <h2 className="mb-2 font-semibold text-xl">Priser</h2>
+                <p>
+                  <strong>Onlinecoaching:</strong>
+                  <li>
+                    30 min konsultation där vi går igenom din träningshistorik,
+                    nuläge, mål mm. Detta för att kunna få fram ett
+                    träningsprogram som är anpassat efter dig och dina
+                    förutsättningar.
+                  </li>
+                  <li className="mt-3">
+                    Träningsprogram i 12 veckor med detaljer som exempelvis
+                    distans, fart & pulszon/ansträngningsnivå.
+                  </li>
+                  <li className="mt-3">
+                    Check-ins: vi träffas online en gång i månaden och
+                    utvärderar månaden som gått och gör eventuella förändringar
+                    i ditt program.
+                  </li>
+                  <p className=" font-semibold mt-3 mb-10"> Pris: 1500 kr</p>
+                </p>
+                <p>
+                  <strong className="">Privat coaching:</strong>
+                  <li>
+                    30 min konsultation där vi går igenom din träningshistorik,
+                    nuläge, mål mm. Detta för att kunna få fram ett
+                    träningsprogram som är anpassat efter dig och dina
+                    förutsättningar.
+                  </li>
+                  <li className="mt-3">
+                    Träningsprogram i 12 veckor med detaljer som exempelvis
+                    distans, fart & pulszon/ansträngningsnivå.
+                  </li>
+                  <li className="mt-3">1 löpanalys med filmning</li>
+                  <li className="mt-3">2 PT-timmar</li>
+                  <li className="mt-3">
+                    Check-ins: vi träffas en gång i månaden och utvärderar
+                    månaden som gått och gör eventuella förändringar i ditt
+                    program.
+                  </li>
+                  <p className=" font-semibold mt-3">Pris: 2100 kr</p>
+                </p>
+              </div>
+            </section>
+          </div>
 
           <div className="text-center flex justify-center mt-16 mb-5">
             <Link to="/coach" className="inline-flex">
